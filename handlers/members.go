44fdd3ea1b2cb7a064ea15997a8062082fe834dd
Kitaminka <kit4minka@outlook.com>
2022-03-28 22:14:55 +0300

    Add fields to welcome message

--- handlers/members.go
+++ handlers/members.go
@@ -12,20 +12,33 @@ func GuildMemberAdd(discordSession *discordgo.Session, guildMemberAdd *discordgo
 
 	_, err := discordSession.ChannelMessageSendComplex("957693593693339698", &(discordgo.MessageSend{
 		Content: fmt.Sprintf("<@%v>", guildMemberAdd.User.ID),
-		Embeds: []*discordgo.MessageEmbed{{
-			URL:         "",
-			Title:       fmt.Sprintf("Welcome, %v!", guildMemberAdd.User.Username),
-			Description: fmt.Sprintf("Welcome to the %v server!", guild.Name),
-			Timestamp:   "",
-			Color:       8523465,
-			Footer:      nil,
-			Image:       nil,
-			Thumbnail:   nil,
-			Video:       nil,
-			Provider:    nil,
-			Author:      nil,
-			Fields:      nil,
-		}},
+		Embeds: []*discordgo.MessageEmbed{
+			{
+				URL:         "",
+				Title:       fmt.Sprintf("Welcome, **%v**!", guildMemberAdd.User.Username),
+				Description: fmt.Sprintf("Welcome to the **%v** server!", guild.Name),
+				Timestamp:   "",
+				Color:       8523465,
+				Footer:      nil,
+				Image:       nil,
+				Thumbnail:   nil,
+				Video:       nil,
+				Provider:    nil,
+				Author:      nil,
+				Fields: []*discordgo.MessageEmbedField{
+					{
+						Name:   "❔What is this server?",
+						Value:  "This is a unique server that implements a game in which you are a citizen of the city. Chat, earn money, get a unique gaming experience!",
+						Inline: false,
+					},
+					{
+						Name:   "👤How to start?",
+						Value:  "Create a profile and start chatting, playing!",
+						Inline: false,
+					},
+				},
+			},
+		},
 		TTS: false,
 		Components: []discordgo.MessageComponent{discordgo.ActionsRow{Components: []discordgo.MessageComponent{discordgo.Button{
 			Label:    "Create profile",
